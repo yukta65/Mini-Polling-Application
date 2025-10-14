5e1c584dd16f21fffb275ff3d328e4b8636c83df
yukta65 <yuktamahedu652004@gmail.com>
2025-10-14 11:21:02 +0530

    update

--- polling-backend/models/option.js
+++ polling-backend/models/option.js
@@ -1,8 +1,24 @@
+// Import DataTypes from Sequelize to define column data types
 const { DataTypes } = require("sequelize");
+
+// Import the configured Sequelize instance (database connection)
 const sequelize = require("../config/db");
 
+// ----------------------- Option Model Definition -----------------------
+
+// Define the Option model representing poll answer choices in the database
+// Sequelize will automatically create a table named 'Options' (pluralized)
+// It will also automatically add: id (primary key), createdAt, updatedAt, pollId (foreign key)
 const Option = sequelize.define("Option", {
-  text: { type: DataTypes.STRING, allowNull: false },
+  // text field: stores the option text/label (e.g., "Yes", "No", "Maybe")
+  text: {
+    type: DataTypes.STRING, // VARCHAR data type in SQL
+    allowNull: false, // This field is required (NOT NULL constraint)
+  },
+  // Note: pollId foreign key is automatically created by the Poll.hasMany(Option) association
+  // defined in models/index.js
 });
 
+// Export the Option model so it can be used in other parts of the application
+// for creating, querying, updating, and deleting poll options
 module.exports = Option;
--- polling-backend/models/poll.js
+++ polling-backend/models/poll.js
@@ -1,15 +1,32 @@
+// Import DataTypes from Sequelize to define column data types
 const { DataTypes } = require("sequelize");
+
+// Import the configured Sequelize instance (database connection)
 const sequelize = require("../config/db");
 
+// ----------------------- Poll Model Definition -----------------------
+
+// Define the Poll model representing surveys/polls in the database
+// Sequelize will automatically create a table named 'Polls' (pluralized)
+// It will also automatically add: id (primary key), createdAt, updatedAt
 const Poll = sequelize.define("Poll", {
+  // question field: stores the main poll question
+  // Example: "What is your favorite programming language?"
   question: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    type: DataTypes.STRING, // VARCHAR data type in SQL
+    allowNull: false, // This field is required (NOT NULL constraint)
   },
+
+  // expiresAt field: optional expiration date/time for the poll
+  // Can be used to automatically close polls after a certain date
   expiresAt: {
-    type: DataTypes.DATE,
-    allowNull: true,
+    type: DataTypes.DATE, // DATETIME data type in SQL
+    allowNull: true, // This field is optional (can be NULL)
   },
+  // Note: Related options are accessed through the Poll.hasMany(Option) association
+  // defined in models/index.js (e.g., poll.options)
 });
 
+// Export the Poll model so it can be used throughout the application
+// for creating, querying, updating, and deleting polls
 module.exports = Poll;
